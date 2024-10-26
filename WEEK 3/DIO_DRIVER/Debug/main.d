43938b0a4185cc59161888bf6fb776b5fa657d0e
MostafaSaad21 <158571197+MostafaSaad21@users.noreply.github.com>
2024-10-26 23:51:09 +0300

    Delete WEEK 3/DIO_DRIVER directory

--- WEEK 3/DIO_DRIVER/Debug/main.d
+++
@@ -1,7 +0,0 @@
-main.o main.o: ../main.c ../init.h ../Stdh_types.h ../ERROR.h
-
-../init.h:
-
-../Stdh_types.h:
-
-../ERROR.h:
--- WEEK 3/DIO_DRIVER/Stdh_types.h
+++
@@ -1,7 +0,0 @@
-#ifndef STD_TYPES_H
-#define STD_TYPES_H
-
-
-typedef unsigned char u8;
-
-#endif
